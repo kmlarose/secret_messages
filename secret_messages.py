1962f1c14026fe95336a325d6440e738ad158455
Ken LaRose <kmlarose2@gmail.com>
2017-05-29 16:10:48 -0500

    rudimentary console UI

--- secret_messages.py
+++ secret_messages.py
@@ -1,19 +1,53 @@
+from affine import Affine
 from alberti import Alberti
 from caesar import Caesar
+import os
 
 
-secret_message = 'how does this look encrypted?'
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
 
-# test the Caesar Cypher
-print('Caesar Test')
-print('='*11)
-julius = Caesar()
-print(julius.encrypt('abc'))
-print(julius.decrypt('def'))
 
-# test the Alberti Cypher
-print('\nAlberti Test')
-print('='*12)
-cipher = Alberti()
-print(cipher.encrypt('what happens here'))
-print(cipher.decrypt('WDDAZWZZKRRUQSHMJONVVVBQXYXULWR'))
+def run_console_ui():
+    main_menu = [
+        '1: Caesar Cipher',
+        '2: Alberti Cipher',
+        '3: Affine Cipher',
+        '0: Quit'
+    ]
+    while True:
+        clear()
+        print('Secret Messages!')
+        print('=' * 16)
+        print('Which cipher would you like to use?:')
+        for option in main_menu:
+            print(option)
+
+        cipher_choice = input('> ')
+        if cipher_choice == '1':
+            # test the Caesar Cypher
+            print('Caesar Test')
+            print('=' * 11)
+            julius = Caesar()
+            print(julius.encrypt('abc'))
+            print(julius.decrypt('def'))
+            input('press any key to continue...')
+        elif cipher_choice == '2':
+            # test the Alberti Cypher
+            print('\nAlberti Test')
+            print('=' * 12)
+            alb_ciph = Alberti()
+            print(alb_ciph.encrypt('what happens here'))
+            print(alb_ciph.decrypt('WDDAZWZZKRRUQSHMJONVVVBQXYXULWR'))
+            input('press any key to continue...')
+        elif cipher_choice == '3':
+            affine = Affine()
+            print('Affine test... {} work in progress'.format(affine))
+            input('press any key to continue...')
+        elif cipher_choice == '0':
+            print('bye!')
+            break
+
+
+if __name__ == '__main__':
+    run_console_ui()
